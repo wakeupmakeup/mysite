7379507f99c90938597db02bc4a29de13016a8c6
wakeupmakeup <zerostar998@naver.com>
2024-03-10 17:25:29 +0900

    [ADD] Nav, Banner, skills, projects

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './components/NavBar.js';
+import Banner from './components/Banner.js';
+import { Skills } from './components/Skills.js';
+import { Projects } from "./components/Projects";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Banner />
+      <Skills />
+      <Projects />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Banner.js
@@ -0,0 +1,62 @@
+import { Container, Row, Col } from "react-bootstrap";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import headerImg from "../assets/img/header-img.svg";
+import { useEffect, useState } from "react";
+
+
+
+export default function Banner() {
+    const [loopNum, setLoopNum] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const toRotate = ["iOS Developer", "Web Designer", "UI/UX Designer"];
+    const [text, setText] = useState('');
+    const period = 2000;
+    const [delta, setDelta] = useState(300 - Math.random() * 100);
+
+    useEffect(() => {
+        let ticker = setInterval(() => {
+            tick();
+        }, delta)
+
+        return () => { clearInterval(ticker) };
+    }, [text])
+
+    const tick = () => {
+        let i = loopNum % toRotate.length;
+        let fullText = toRotate[i];
+        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+        setText(updatedText);
+
+        if (isDeleting) {
+            setDelta(prevDelta => prevDelta / 2)
+        }
+
+        if (!isDeleting && updatedText === fullText) {
+            setIsDeleting(true);
+            setDelta(period);
+        } else if (isDeleting && updatedText === '') {
+            setIsDeleting(false);
+            setLoopNum(loopNum + 1);
+            setDelta(500);
+        }
+    }
+
+    return (
+        <section className="banner" id="home">
+            <Container>
+                <Row className="align-items-center">
+                    <Col xs={12} md={6} xl={7}>
+                        <span className="tagline">안녕하세요 반갑습니다.</span>
+                        <h1>{"안녕하세요"}<span className="wrap">{text}</span></h1>
+                        <p>Lorem Ipsh Lorem Ipsh Lorem Ipsh Lorem IpshLorem IpshLorem IpshLorem IpshLorem IpshLorem IpshLorem Ipsh</p>
+                        <button onClick={() => console.log('coonect')}>연락주세요 <ArrowRightCircle size={25} /> </button>
+                    </Col>
+                    <Col xs={12} md={6} xl={5}>
+                        <img src={headerImg} alt="Headder Img" />
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    )
+}
